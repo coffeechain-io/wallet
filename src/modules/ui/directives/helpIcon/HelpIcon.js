697e61bdd2295c305989faae7e8a715a26a0c816
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2018-07-13 15:17:35 +1000

    COF_wallet

--- src/modules/ui/directives/helpIcon/HelpIcon.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('app.ui').component('wHelpIcon', {
-        templateUrl: 'modules/ui/directives/helpIcon/helpIcon.html',
-        transclude: true
-    });
-
-})();
